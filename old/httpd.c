1a7a4e322525fdfbb3d33282da2d70afacb9d125
No_World <2259881867@qq.com>
2025-05-19 17:01:09 +0800

    modified:   old/httpd.c

--- old/httpd.c
+++ old/httpd.c
@@ -1,10 +1,10 @@
 #include <arpa/inet.h>
-#include <cstring>
 #include <ctype.h>
-#include <iostream>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
